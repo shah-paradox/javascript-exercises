8a169d45ab1bc2ae6f4e95cb4be9d0162aa1ded5
shah-paradox <[shahmeeramircoder@gmail.com]>
2024-08-02 23:18:48 +0500

    complete: Learnt infinite argument technique
    So arguments in an object in every function that acts like an array but is not an
    array. We can utilize it to access all arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for(let i =0; i<arr.length;i++)
+    {
+        let flag=false;
+        for(let j=1;j<arguments.length;j++)
+        {
+            if(arguments[j]===arr[i])
+                flag=true;
+        }
+        if(flag==true)
+        {
+            arr.splice(i,1);
+            i--;
+        }
+        
+    }
+    console.log(arguments[2]);
+    return arr;
 };
 
 // Do not edit below this line
