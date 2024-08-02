da58a852c1c161280bb854c07652893c1e6698bc
shah-paradox <[shahmeeramircoder@gmail.com]>
2024-08-02 22:41:42 +0500

    complete: 3 excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let l=str.length;
+    let res="";
+    for(let i =l-1; i>=0;i--)
+    {
+        res+=str[i];
+    }
+    
+    return res;
 };
 
 // Do not edit below this line
